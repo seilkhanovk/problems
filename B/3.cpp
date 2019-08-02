412378731af3491a5951fb08bbf5345c9ba58813
seilkhanovkkk <seil.kuka@gmail.com>
2019-08-02 16:24:17 +0600

    3 solved

---
+++ B/3.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int start = -1, maxLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (dict[s[i]] > start) start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
